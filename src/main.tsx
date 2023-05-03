a2e76a22f91b42d7077f9e5b870747ce1ef5a39a
giwan <giwan.dev@gmail.com>
2023-05-03 21:54:44 +0900

    🎨 main.tsx에서 root를 as로 강제하는 코드 제거
    as 없이 표현합니다.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RootRouter } from "./router";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const rootEl = document.getElementById("root");
+
+if (rootEl === null) {
+  throw new Error("root를 ID로 가진 엘리먼트가 없습니다.");
+}
+
+ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
     <RootRouter />
   </React.StrictMode>
